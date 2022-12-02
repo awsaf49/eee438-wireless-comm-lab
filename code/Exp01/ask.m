4ceea9dd1350c0adcdad74f6234007cfbd9a50fb
Awsaf <awsaf49@gmail.com>
2022-12-02 10:51:16 +0600

    refactro

--- code/Exp01/ask.m
+++ code/Exp01/ask.m
@@ -1,18 +1,33 @@
 clc;
 clear all;
 close all;
-bits = randi([0,1], 1, 500);
-bits = repelem(bits, 250);
-t=linspace(0, 0.5, 500*250);
+N=500;
+A0=5;
+A1=10;
+fc=5e3;
+Rb = 1000;
+Tb = 1/Rb;
+Fs = 40*fc;
+Ts = 1/Fs;
+k = Fs*Tb;
+
+
+bits = randi([0,1], 1, N);
+bits = repelem(bits, k);
+t= 0:Ts:N*Tb-Ts; %linspace(0, 0.5, N*k);
 figure;
 subplot(211);
 stairs(t,bits)
 % xaxis([0, 0.005]);
-A0=5;
-A1=10;
-fc=5e3;
 cbits = 1.-bits;
 y = bits.*(A1*sin(2*pi*fc*t)) + cbits.*(A0*sin(2*pi*fc*t));
 % figure;
 subplot(212);
 plot(t, y);
+
+figure;
+[pxx f] = pwelch(y, 500, 200, 500, Fs);
+pxx = pxx/max(pxx);
+plot(f, pxx);
+xlim([0, 10000]);
+ylim([-0.1, 1]);
